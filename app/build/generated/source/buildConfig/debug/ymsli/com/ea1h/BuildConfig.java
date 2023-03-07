a31c9d24136cb782c7d76d4103dc938dba1510a2
Utkarsh Sharma <52097056+ke-utkarsh@users.noreply.github.com>
2023-03-07 12:34:25 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/ymsli/com/ea1h/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ymsli.com.ea1h;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ymsli.com.ea1h";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 116;
+  public static final String VERSION_NAME = "2.9";
+  // Field from build type: debug
+  public static final String BASE_URL = "https://jf40138cu3.execute-api.ap-south-1.amazonaws.com/prod";
+  // Field from build type: debug
+  public static final String DAP_IOT_URL = "https://www.be0015520f-eu.com/";
+  // Field from build type: debug
+  public static final String OS = "android";
+}
