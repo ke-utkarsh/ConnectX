6d828786e95e8ad48e25177b05cff16f53a30370
Utkarsh Sharma <52097056+ke-utkarsh@users.noreply.github.com>
2023-03-07 11:12:33 +0530

    Add files via upload

---
+++ pmp3lib/build/generated/source/buildConfig/release/ymsli/com/demoea1hlib/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ymsli.com.demoea1hlib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "ymsli.com.demoea1hlib";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "ymsli.com.demoea1hlib";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
